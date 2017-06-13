61a9cd27c775881716f3a80301056758b4ae5c88
Anuradha Dissanayake <falconne@gmail.com>
2017-06-13 21:31:13 +1200

    Initial WIP

---
+++ HeatMap.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace HeatMap
+{
+    internal class HeatMap : ICellBoolGiver
+    {
+        private CellBoolDrawer _drawerInt;
+
+        private readonly Map _map;
+
+        public HeatMap(Map map)
+        {
+            _map = map;
+        }
+
+        public CellBoolDrawer Drawer
+        {
+            get
+            {
+                if (_drawerInt == null)
+                {
+                    _drawerInt = new CellBoolDrawer(this, _map.Size.x, _map.Size.z, 0.33f);
+                }
+                return _drawerInt;
+            }
+        }
+
+        public bool GetCellBool(int index)
+        {
+            return true;
+        }
+
+        public Color GetCellExtraColor(int index)
+        {
+            return Color.white;
+        }
+
+        public Color Color
+        {
+            get
+            {
+                return new Color(0.3f, 1f, 0.4f);
+            }
+        }
+
+    }
+}
--- Main.cs
+++ Main.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HugsLib.Utils;
+using Verse;
 
 namespace HeatMap
 {
-    public class Main
+    public class Main : HugsLib.ModBase
     {
+        public Main()
+        {
+            Instance = this;
+        }
+
+        public override void MapLoaded(Map map)
+        {
+            base.MapLoaded(map);
+            heatMap = new HeatMap(map);
+        }
+
+        internal new ModLogger Logger => base.Logger;
+
+        internal static Main Instance { get; private set; }
+
+        public override string ModIdentifier => "HeatMap";
+
+        private HeatMap heatMap;
     }
 }
